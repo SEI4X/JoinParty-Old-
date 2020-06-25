acac4cc96ae7327a0b556d3b206b790cc397c3d5
SEI4X <mashkov0599@gmail.com>
2020-06-25 16:13:59 +0300

    Project created

---
+++ Join PartyTests/Join_PartyTests.swift
@@ -0,0 +1,34 @@
+//
+//  Join_PartyTests.swift
+//  Join PartyTests
+//
+//  Created by Alexei Mashkov on 25.06.2020.
+//  Copyright © 2020 Alexei Mashkov. All rights reserved.
+//
+
+import XCTest
+@testable import Join_Party
+
+class Join_PartyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
